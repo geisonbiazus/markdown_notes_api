78d51be07e2d622498e4695c8c97df58ccbb37e5
Geison Biazus <geisonbiazus@gmail.com>
2018-03-04 22:04:29 -0300

    Merge all note usecases in one

---
+++ internal/markdownnotes/notes/note_use_case.go
@@ -0,0 +1,64 @@
+package notes
+
+import (
+	"github.com/geisonbiazus/markdown_notes_api/internal/markdownnotes"
+	"github.com/geisonbiazus/markdown_notes_api/internal/markdownnotes/notes/validators"
+)
+
+type NoteUseCase struct {
+	NoteStorage markdownnotes.NoteStorage
+	Validator   validators.NoteValidator
+}
+
+func NewNoteUseCase(storage markdownnotes.NoteStorage) *NoteUseCase {
+	return &NoteUseCase{
+		NoteStorage: storage,
+		Validator:   validators.NoteValidator{},
+	}
+}
+
+func (u *NoteUseCase) CreateNote(title, content string, presenter markdownnotes.NotePresenter) error {
+	note := markdownnotes.Note{
+		Title:   title,
+		Content: content,
+	}
+
+	errs := u.Validator.Validate(note)
+	if len(errs) > 0 {
+		presenter.PresentError(errs)
+		return nil
+	}
+
+	note, err := u.NoteStorage.Save(note)
+	if err != nil {
+		return err
+	}
+	presenter.PresentNote(note)
+
+	return nil
+}
+
+func (u *NoteUseCase) ListNotes(presenter markdownnotes.NoteListPresenter) error {
+	notes, err := u.NoteStorage.FindAll()
+	if err != nil {
+		return err
+	}
+	presenter.PresentNotes(notes)
+	return nil
+}
+
+func (u *NoteUseCase) ShowNote(noteID int, presenter markdownnotes.NotePresenter) error {
+	note, err := u.NoteStorage.FindByID(noteID)
+	if err != nil {
+		return err
+	}
+
+	if note == markdownnotes.NoNote {
+		presenter.NotFound()
+		return nil
+	}
+
+	presenter.PresentNote(note)
+
+	return nil
+}
---
+++ internal/markdownnotes/notes/note_use_case_test.go
@@ -0,0 +1,199 @@
+package notes
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/geisonbiazus/markdown_notes_api/internal/markdownnotes"
+	"github.com/geisonbiazus/markdown_notes_api/internal/testing/doubles"
+)
+
+func TestNoteUseCase(t *testing.T) {
+	t.Run("CreateNote", func(t *testing.T) {
+		setup := func() (
+			markdownnotes.Note,
+			*doubles.NoteStorageSpy,
+			*doubles.NotePresenterSpy,
+			*NoteUseCase,
+		) {
+			note := markdownnotes.Note{
+				Title:   "Note Title",
+				Content: "# Note Content",
+			}
+			storage := doubles.NewNoteStorageSpy()
+			presenter := doubles.NewNotePresenterSpy()
+			usecase := NewNoteUseCase(storage)
+
+			return note, storage, presenter, usecase
+		}
+
+		t.Run("Given valid arguments, it creates and presents a note", func(t *testing.T) {
+			note, storage, presenter, usecase := setup()
+			savedNote := markdownnotes.Note{
+				ID:      1,
+				Title:   note.Title,
+				Content: note.Content,
+			}
+
+			storage.SaveNoteResult = savedNote
+
+			usecase.CreateNote(note.Title, note.Content, presenter)
+
+			if storage.SaveNoteArg != note {
+				t.Errorf("Expected: %v. Actual: %v", note, storage.SaveNoteArg)
+			}
+
+			if presenter.PresentNoteNoteArg != savedNote {
+				t.Errorf("Expected: %v. Actual: %v", savedNote, presenter.PresentNoteNoteArg)
+			}
+		})
+
+		t.Run("Given an error occurs on create, it returns the error", func(t *testing.T) {
+			note, _, presenter, usecase := setup()
+			storage := doubles.NewErrorNoteSotorageStub()
+
+			usecase.NoteStorage = storage
+
+			err := usecase.CreateNote(note.Title, note.Content, presenter)
+
+			if storage.Error != err {
+				t.Errorf("Expected: %v. Actual: %v", storage.Error, err)
+			}
+		})
+
+		t.Run("Given an empty title, it validates note and presents the error", func(t *testing.T) {
+			_, _, presenter, usecase := setup()
+
+			errs := []markdownnotes.ValidationError{
+				markdownnotes.ValidationError{
+					Field:   "title",
+					Message: "Can't be blank",
+					Type:    "REQUIRED",
+				},
+			}
+
+			usecase.CreateNote("", "", presenter)
+
+			if !reflect.DeepEqual(presenter.PresentErrorErrsArg, errs) {
+				t.Errorf("Expected: %v. Actual: %v", presenter.PresentErrorErrsArg, errs)
+			}
+		})
+	})
+
+	t.Run("ListNotes", func(t *testing.T) {
+		setup := func() (
+			*doubles.NoteStorageSpy,
+			*NoteUseCase,
+			*doubles.NoteListPresenterSpy,
+		) {
+			storage := doubles.NewNoteStorageSpy()
+			usecase := NewNoteUseCase(storage)
+			presenter := doubles.NewNoteListPresenterSpy()
+			return storage, usecase, presenter
+		}
+
+		t.Run("Given no notes, it presents an empty notes list", func(t *testing.T) {
+			_, usecase, presenter := setup()
+			usecase.ListNotes(presenter)
+
+			if !presenter.PresentNotesCalled {
+				t.Error("It didn't call PresentNotes")
+			}
+
+			if len(presenter.PresentNotesNotesArg) > 1 {
+				t.Error("It presented some notes when it shouldn't")
+			}
+		})
+
+		t.Run("Given a saved note, it presentes a list containing it", func(t *testing.T) {
+			storage, usecase, presenter := setup()
+
+			notes := []markdownnotes.Note{
+				markdownnotes.Note{Title: "Title", Content: "Content"},
+			}
+
+			storage.FindAllResult = notes
+			usecase.ListNotes(presenter)
+
+			if !presenter.PresentNotesCalled {
+				t.Error("It didn't call PresentNotes")
+			}
+
+			if !reflect.DeepEqual(presenter.PresentNotesNotesArg, notes) {
+				t.Errorf("Expected: %v. Actual: %v", notes, presenter.PresentNotesNotesArg)
+			}
+		})
+
+		t.Run("Given an error on find, it returns the error", func(t *testing.T) {
+			_, usecase, presenter := setup()
+			storage := doubles.NewErrorNoteSotorageStub()
+			usecase.NoteStorage = storage
+
+			err := usecase.ListNotes(presenter)
+
+			if err != storage.Error {
+				t.Errorf("Expected: %v. Actual: %v", storage.Error, err)
+			}
+		})
+	})
+
+	t.Run("ShowNote", func(t *testing.T) {
+		setup := func() (
+			*doubles.NoteStorageSpy,
+			*NoteUseCase,
+			*doubles.NotePresenterSpy,
+		) {
+			storage := doubles.NewNoteStorageSpy()
+			usecase := NewNoteUseCase(storage)
+			presenter := doubles.NewNotePresenterSpy()
+			return storage, usecase, presenter
+		}
+
+		t.Run("Given an existent note ID, it fetchs and presents the note", func(t *testing.T) {
+			storage, usecase, presenter := setup()
+
+			note := markdownnotes.Note{ID: 1, Title: "Title"}
+			storage.FindByIDNoteResult = note
+
+			usecase.ShowNote(note.ID, presenter)
+
+			if storage.FindByIDIDArg != note.ID {
+				t.Errorf("Expected: %v. Actual: %v", note.ID, storage.FindByIDIDArg)
+			}
+
+			if presenter.PresentNoteNoteArg != note {
+				t.Errorf("Expected: %v. Actual: %v", note, presenter.PresentNoteNoteArg)
+			}
+		})
+
+		t.Run("Given a non existing note ID, it presents note not found", func(t *testing.T) {
+			storage, usecase, presenter := setup()
+
+			noteID := 42
+
+			usecase.ShowNote(noteID, presenter)
+
+			if storage.FindByIDIDArg != noteID {
+				t.Errorf("Expected: %v. Actual: %v", noteID, storage.FindByIDIDArg)
+			}
+
+			if !presenter.NotFoundCalled {
+				t.Error("It didn't call NotFound")
+			}
+		})
+
+		t.Run("When an error occur on find, it returns the error", func(t *testing.T) {
+			_, usecase, presenter := setup()
+			storage := doubles.NewErrorNoteSotorageStub()
+			usecase.NoteStorage = storage
+
+			noteID := 42
+
+			err := usecase.ShowNote(noteID, presenter)
+
+			if err != storage.Error {
+				t.Errorf("Expected: %v. Actual: %v", err, storage.Error)
+			}
+		})
+	})
+}
